fe4e03f08faadae04e2c8d7645e8f167e88a6cab
ArdenB <arden.burrell@unsw.edu.au>
2018-03-31 16:55:08 +1100

    adding new functionality to fix grids

--- Step3_gridrepair.py
+++ Step3_gridrepair.py
@@ -5,6 +5,11 @@ GRID repair
 Takes The outputted netcdf files and makes the grids correct so they work with cdo remap
 
 """
+__title__ = "Netcdf CMIP5 grid repair"
+__author__ = "Arden Burrell"
+__version__ = "1.0 (31.03.2018)"
+__email__ = "arden.burrell@gmail.com"
+
 
 import numpy as np 
 import pandas as pd
@@ -12,11 +17,23 @@ import pdb
 
 
 def main():
-	# Load in the lookup table of the CMIP5 grids
+	# ========== Load in the lookup table of the CMIP5 grids ==========
 	# SOURCE: https://portal.enes.org/data/enes-model-data/cmip5/resolution
-	grids = pd.read_csv("./CMIP5model_lookup.csv", index_col=0)
+	grids     = pd.read_csv("./CMIP5model_lookup.csv", index_col=0)
+
+	# ========== Set up the Key Infomation ==========
+	# the downloaded senarios
+	senarios  = ["historical_rcp8.5", "historical_rcp4.5", "historical_rcp2.6"]
+	# the downloaded variables
+	variables = ["pr", "tas"]
+
+
+
 	pdb.set_trace()
 	pass
+
+
+
 
 
 if __name__ == '__main__':
