34066125b2057a3b79efbe807481c4736b88c478
ArdenB <arden.burrell@unsw.edu.au>
2018-03-31 17:18:36 +1100

    adding new functionality to fix grids

--- Step3_gridrepair.py
+++ Step3_gridrepair.py
@@ -5,23 +5,24 @@ GRID repair
 Takes The outputted netcdf files and makes the grids correct so they work with cdo remap
 
 """
+#==============================================================================
 __title__ = "Netcdf CMIP5 grid repair"
 __author__ = "Arden Burrell"
 __version__ = "1.0 (31.03.2018)"
 __email__ = "arden.burrell@gmail.com"
 
-
+#==============================================================================
 import numpy as np 
 import pandas as pd
 import pdb
 import sys
+import os
+#==============================================================================
 
-
-def main():
+def main(args):
 	# ========== Load in the lookup table of the CMIP5 grids ==========
 	# SOURCE: https://portal.enes.org/data/enes-model-data/cmip5/resolution
-	grids     = pd.read_csv("./CMIP5model_lookup.csv", index_col=0)
-
+	grids     = pd.read_csv("./CMIP5model_lookup.csv", header=0)
 	# ========== Set up the Key Infomation ==========
 	# the downloaded senarios
 	senarios  = ["historical_rcp8.5", "historical_rcp4.5", "historical_rcp2.6"]
@@ -33,18 +34,22 @@ def main():
 	for sen in senarios:
 		for var in variables:
 			for model in grids["Model"]:
-				print (model)
+				repair_netcdf(sen, var, model, grids, args.force)
 			sys.exit()
 
 
-
-
+#==============================================================================
+def repair_netcdf(sen, var, model, grids, force):
+	"""
+	Repairs the grid of a given netcdf file
+	"""
+	# ========== Set the path and the file name ==========
+	path  = "./Processed_CMIP5_data/%s/%s/%s" % (sen, var, model)
+	fname = "%s_%s_%s_r1i1p1_mm_month_1950_2050_%s_regrid.nc" %(var, model, sen, sen)
 	pdb.set_trace()
-	pass
 
 
-
-
+#==============================================================================
 
 if __name__ == '__main__':
 	main()
