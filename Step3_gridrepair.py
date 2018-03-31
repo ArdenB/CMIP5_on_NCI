3e0f7e0fe2d515845abd30c9fa012594907008a6
ArdenB <arden.burrell@unsw.edu.au>
2018-03-31 20:25:53 +1100

    adding new functionality to fix grids+

--- Step3_gridrepair.py
+++ Step3_gridrepair.py
@@ -108,16 +108,17 @@ def repair_netcdf(sen, var, model, grids, force):
 	new_grid.append('xfirst    = -180')
 	new_grid.append('xinc      = %s' %  str(
 		float(grids[grids["Model"]==model]["Longitude"]) ))
-	# Check and see if the start is known
-	# if not (any([n.startswith("yfirst") for n in ginfo])):
-	# 	print ("Its in the loop")
-	# 	for ginf in ginfo:
-	# 		if  ginf.startswith("yvals"):
-	# 			new_grid.append('yfirst      = -90')
+	Check and see if the start is known
+	if not (any([n.startswith("yfirst") for n in ginfo])):
+		print ("Its in the loop")
+		for ginf in ginfo:
+			if  ginf.startswith("yvals"):
+				pdb.set_trace()
+				# new_grid.append('yfirst      = -90')
 				# new_grid.append('yfirst      = %s' %   ginf.split()[2])
 	if not any([n.startswith("yinc") for n in ginfo]):
 		new_grid.append('yinc      = %s' %  str(
-			-float(grids[grids["Model"]==model]["Latitude"]) ))
+			float(grids[grids["Model"]==model]["Latitude"]) ))
 
 	# Save the grid out
 	save_grid(path, new_grid)
